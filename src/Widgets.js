2fe2f0b646d4712a5fa832734fafe21c6dc7e309
Jiei <hamuhamueiji@gmail.com>
2020-12-23 21:00:49 +0900

    よかったねランキング作成に取り掛かった。

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Widgets.css";
+import Post from "./Post";
+import "./Feed.css";
+import SearchIcon from "@material-ui/icons/Search";
+import db from "./firebase";
+import FlipMove from "react-flip-move";
 
-function Widgets() {
+
+  function Widgets() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map((doc) => doc))
+        ))
+    }, []);
+    const N = posts.length;
+
     return (
         <div className="widgets">
-            <div className="widgets__input">
-                <input type="text" />
+            <div className="widget__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
             </div>
-            <a className="widgets__gitbutton" href="https://github.com/Jieijiei/Twitter-clone" target="_blank">製作者のいえ</a>
+
+            <div className="widgets__widgetContainer">
+            <h2>よかったねランキング</h2>
+
+            <FlipMove>
+            {posts.map((post) => (
+                <Post
+                    id={post.id}
+                    displayName={post.data().displayName}
+                    username={post.data().username}
+                    verified={post.data().verified}
+                    text={post.data().text}
+                    avatar={post.data().avatar}
+                    image={post.data().image}
+                    favoritecount={post.data().favoritecount}
+                    time={post.data().time}
+            />
+            ))}
+            </FlipMove>
+           </div>
+           <a className="widgets__gitbutton" href="https://github.com/Jieijiei/Twitter-clone" target="_blank">製作者のいえ</a>
         </div>
     );
 }
